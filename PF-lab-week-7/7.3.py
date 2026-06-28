583ab41847f3d9f432972bd2c8f41bc19c67fca6
Soban Haider <chsoban0066@gmail.com>
2026-06-28 12:25:14 +0500

    Add files via upload

---
+++ PF-lab-week-7/7.3.py
@@ -0,0 +1,15 @@
+
+def printMove(fr, to):
+    print('move from ' + str(fr) + ' to ' + str(to))
+
+
+def Towers(n, fr, to, spare):
+    if n == 1:
+        printMove(fr, to)
+    else:
+        Towers(n-1, fr, spare, to)
+        Towers(1, fr, to, spare)
+        Towers(n-1, spare, to, fr)
+
+tower = Towers(2, 1, 3, 1)
+print(tower)
